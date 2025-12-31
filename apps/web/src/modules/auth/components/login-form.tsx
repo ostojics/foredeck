e1d1dc1e66ea2036af82e11f5cd3a071ced09f7b
ostojics <ostojic.dev@gmail.com>
2025-12-31 12:16:13 +0100

    remove stepper

--- apps/web/src/modules/auth/components/login-form.tsx
+++ apps/web/src/modules/auth/components/login-form.tsx
@@ -4,7 +4,6 @@ import {Button} from '@/components/button/button';
 import {FormLabel} from '@/components/form-label/form-label';
 import {FormControl} from '@/components/form-control/form-control';
 import {FormErrorMessage} from '@/components/form-error-message/form-error-message';
-import {Stepper} from '@/components/stepper/stepper';
 
 const LoginPage = () => {
   const {
@@ -41,15 +40,6 @@ const LoginPage = () => {
 
         <Button type="submit">Login</Button>
       </form>
-
-      <Stepper
-        steps={[
-          {title: 'Step 1'},
-          {title: 'Step 2', description: 'hello'},
-          {title: 'Step 3', description: 'Description for step 3'},
-        ]}
-        currentStep={1}
-      />
     </section>
   );
 };
